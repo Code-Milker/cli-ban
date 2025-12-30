a6263371742d4632176d32b43a5bfb434fa6ecba
Milk <tylerf66@gmail.com>
2025-12-29 18:27:44 -0600

    update drizzle kit

--- src/lib/generated/schema.ts
+++ src/lib/generated/schema.ts
@@ -1,45 +1,31 @@
-import {
-  sqliteTable,
-  index,
-  integer,
-  text,
-  numeric,
-  foreignKey,
-} from "drizzle-orm/sqlite-core";
-import { sql } from "drizzle-orm";
+import { sqliteTable, AnySQLiteColumn, index, check, integer, text, numeric, foreignKey } from "drizzle-orm/sqlite-core"
+  import { sql } from "drizzle-orm"
 
-export const tasks = sqliteTable(
-  "tasks",
-  {
-    id: integer("id").primaryKey({ autoIncrement: true }),
-    category: text("category").notNull(),
-    title: text("title").notNull(),
-    section: text("section").default("TODO").notNull(),
-    createdAt: numeric("created_at").default(sql`(CURRENT_TIMESTAMP)`),
-    updatedAt: numeric("updated_at").default(sql`(CURRENT_TIMESTAMP)`),
-  },
-  (table) => {
-    return {
-      idxTasksSection: index("idx_tasks_section").on(table.section),
-    };
-  },
-);
+export const tasks = sqliteTable("tasks", {
+	id: integer().primaryKey({ autoIncrement: true }),
+	category: text().notNull(),
+	title: text().notNull(),
+	section: text().default("TODO").notNull(),
+	createdAt: numeric("created_at").default(sql`(CURRENT_TIMESTAMP)`),
+	updatedAt: numeric("updated_at").default(sql`(CURRENT_TIMESTAMP)`),
+},
+(table) => [
+	index("idx_tasks_section").on(table.section),
+	check("tasks_check_1", sql`category IN ('Job', 'Project', 'Health', 'Friend', 'Fun', 'Life'`),
+	check("tasks_check_2", sql`section IN ('TODO', 'DOING', 'DONE'`),
+	check("task_attributes_check_3", sql`key IN ('goal', 'done', 'remaining', 'note', 'outcome', 'deadline', 'target'`),
+]);
 
-export const taskAttributes = sqliteTable(
-  "task_attributes",
-  {
-    id: integer("id").primaryKey({ autoIncrement: true }),
-    taskId: integer("task_id")
-      .notNull()
-      .references(() => tasks.id, { onDelete: "cascade" }),
-    key: text("key").notNull(),
-    value: text("value").notNull(),
-  },
-  (table) => {
-    return {
-      idxTaskAttributesTaskId: index("idx_task_attributes_task_id").on(
-        table.taskId,
-      ),
-    };
-  },
-);
+export const taskAttributes = sqliteTable("task_attributes", {
+	id: integer().primaryKey({ autoIncrement: true }),
+	taskId: integer("task_id").notNull().references(() => tasks.id, { onDelete: "cascade" } ),
+	key: text().notNull(),
+	value: text().notNull(),
+},
+(table) => [
+	index("idx_task_attributes_task_id").on(table.taskId),
+	check("tasks_check_1", sql`category IN ('Job', 'Project', 'Health', 'Friend', 'Fun', 'Life'`),
+	check("tasks_check_2", sql`section IN ('TODO', 'DOING', 'DONE'`),
+	check("task_attributes_check_3", sql`key IN ('goal', 'done', 'remaining', 'note', 'outcome', 'deadline', 'target'`),
+]);
+
